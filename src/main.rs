5bcd1066f6a91841a0e7a3be6a5c409eab486d05
NegaNote <neganote43@gmail.com>
2025-05-26 17:50:37 -0400

    beginnings, get dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+use winit::event_loop::EventLoop;
+use anyhow::Result;
+use winit::window::{Window, WindowAttributes};
+
+fn main() -> Result<()> {
+    let event_loop = EventLoop::new()?;
     println!("Hello, world!");
+    Ok(())
 }
