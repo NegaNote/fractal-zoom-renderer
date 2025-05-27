c6dff4bb4d86cbf83627ccec2a0c9ab33dafea00
NegaNote <neganote43@gmail.com>
2025-05-26 20:32:06 -0400

    basic window exists

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,71 @@
-use winit::event_loop::EventLoop;
 use anyhow::Result;
-use winit::window::{Window, WindowAttributes};
+use softbuffer::{Context, Surface};
+use std::num::NonZeroU32;
+use std::sync::Arc;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+
+#[derive(Default, Debug)]
+struct App {
+    window: Option<Arc<Window>>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .expect("Failed to create window"),
+        ));
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                let context = Context::new(self.window.as_ref().unwrap().as_ref())
+                    .expect("Failed to create context");
+
+                let window = self.window.as_ref().unwrap().as_ref();
+                let mut surface = Surface::new(&context, window).expect("Failed to create surface");
+                let size = window.inner_size();
+                if let (Some(width), Some(height)) =
+                    (NonZeroU32::new(size.width), NonZeroU32::new(size.height))
+                {
+                    surface
+                        .resize(width, height)
+                        .expect("Failed to resize surface");
+                    let mut buffer = surface.buffer_mut().expect("Failed to get buffer");
+                    (0..height.get()).for_each(|y| {
+                        (0..width.get()).for_each(|x| {
+                            let index = y as usize * width.get() as usize + x as usize;
+                            buffer[index] = (x + y) % 256;
+                        })
+                    });
+
+                    buffer.present().unwrap();
+                }
+            }
+            _ => (),
+        }
+    }
+}
 
 fn main() -> Result<()> {
     let event_loop = EventLoop::new()?;
-    println!("Hello, world!");
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app)?;
     Ok(())
 }
